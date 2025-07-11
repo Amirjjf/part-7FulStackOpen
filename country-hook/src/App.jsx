77d9c61e374f56191cb1b21401fa0994c17488e8
Amirreza Jafarian <amirreza.jfn2000@gmail.com>
2025-07-11 22:05:30 +0300

    First Commit

--- country-hook/src/App.jsx
+++ country-hook/src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
+
+const API_URL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
+
 const useField = (type) => {
   const [value, setValue] = useState('')
 
@@ -16,15 +20,37 @@ const useField = (type) => {
 }
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+  const [country, setCountry] = useState({
+    found: false,
+    data: null
+  })
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`${API_URL}${name}`)
+        .then(response => {
+          setCountry({
+            found: true,
+            data: response.data
+          })
+        })
+        .catch(() => {
+          setCountry({
+            found: false,
+            data: null
+          })
+        })
+    } else {
+      setCountry({ found: false, data: null })
+    }
+  }, [name])
 
   return country
 }
 
 const Country = ({ country }) => {
-  if (!country) {
+  if (!country || !country.data) {
     return null
   }
 
@@ -36,12 +62,16 @@ const Country = ({ country }) => {
     )
   }
 
+  const { name, capital, population, flags } = country.data
+
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{name?.common || 'Unknown'} </h3>
+      <div>capital {capital && capital.length > 0 ? capital[0] : 'N/A'} </div>
+      <div>population {population || 'N/A'}</div> 
+      {flags?.png && (
+        <img src={flags.png} height='100' alt={`flag of ${name?.common || 'country'}`}/>
+      )}
     </div>
   )
 }
@@ -60,9 +90,8 @@ const App = () => {
     <div>
       <form onSubmit={fetch}>
         <input {...nameInput} />
-        <button>find</button>
+        <button type="submit">find</button>
       </form>
-
       <Country country={country} />
     </div>
   )
